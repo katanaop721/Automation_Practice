cbb69888820088fa6c694cd11c4ca299b77b9ddc
Anku Kumar <katanaop6@gmail.com>
2024-12-30 01:48:24 +0530

    .

---
+++ src/test/java/com/qa/Package/TESTNG/Assertions.java
@@ -0,0 +1,29 @@
+package com.qa.Package.TESTNG;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Assertions {
+
+    //@Test
+    void hardassert(){
+        String exp="fgjjw";
+        String act="Hello";
+
+        Assert.assertEquals(act, exp);
+        Assert.assertNotEquals(123,321);
+    }
+
+    @Test
+    void softassert(){
+        String exp="fgjjw";
+        String act="Hello";
+        SoftAssert sa=new SoftAssert();
+        sa.assertEquals(act, exp);
+        System.out.print("Hello testing");
+        //sa.assertNotEquals(321,321);
+        sa.assertAll();
+    }
+
+
+}
