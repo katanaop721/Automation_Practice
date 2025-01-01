512c53e1e26f56023c531a7bfd39fcd048025758
Anku Kumar <katanaop6@gmail.com>
2025-01-02 02:21:15 +0530

    .

---
+++ src/test/java/com/qa/Package/TESTNG/DataProviderExample.java
@@ -0,0 +1,50 @@
+package com.qa.Package.TESTNG;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class DataProviderExample {
+    WebDriver driver;
+
+    @BeforeClass
+    void setup() {
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+        // Navigate to the login page
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @Test(dataProvider = "dp")
+    void login(String username, String password) {
+        driver.findElement(By.xpath("//input[@data-test='username']")).sendKeys(username);
+        driver.findElement(By.xpath("//input[@data-test='password']")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@id='login-button'][@name='login-button']")).click();
+        //driver.findElement(By.xpath("//button[@id='add-to-cart-sauce-labs-fleece-jacket']")).click();
+    }
+
+    @AfterClass
+    void logout() {
+        // driver.findElement(By.xpath("//button[@type='button']")).click();
+        // driver.findElement(By.xpath("//a[@id='logout_sidebar_link']")).click();
+
+        // Close the browser
+        driver.quit();
+    }
+
+    @DataProvider(name = "dp",indices={0,2})
+    Object[][] logindata() {
+         Object data[][]={
+            {"locked_out_user","secret_sauce"},
+            {"problem_user","secret_sauce"},
+            {"performance_glitch_user","secret_sauce"},
+         };
+        return data;
+    }
+}
